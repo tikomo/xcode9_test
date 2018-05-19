43b0b28e33a9f257d6e7190a77f7bafd84a580a7
tikomo <tikomo@gmail.com>
2018-05-19 10:43:48 +0900

    Initial Commit

---
+++ del2/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  del2
+//
+//  Created by Tsuyoshi Komorita on 2018/05/19.
+//  Copyright © 2018年 Tsuyoshi Komorita. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
